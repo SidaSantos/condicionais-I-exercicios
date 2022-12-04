58632346a2bcc044dfc5ab8858f4792698ed2f74
USUÁRIO <sidasantos6395@gmail.com>
2022-12-04 18:57:55 -0300

    exercicio-de-condicionais1

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isTerminouEnsinoMedio;
-let isCursandoOutraFaculdade;
+let idade = 20
+let isTerminouEnsinoMedio = true
+let isCursandoOutraFaculdade = false
 
-if(){
-
-}else{
-	
+if (idade >= 18) {
+    console.log("maior  de idade!")
+} else {
+    console.log("menor de idade")
 }
+
+if (isTerminouEnsinoMedio) {
+    console.log("terminou o ensino medio")
+} else {
+console.log("nao terminou o ensino medio")
+}
+
+if (isCursandoOutraFaculdade) {
+    console.log("cursando faculdade")
+} else {
+console.log("nao esta cursando faculdade")
+}
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual a sua nacionalidade?")
+
+const nacionalidades = ["brasileira", "argentina",
+"uruguaia", "chilena", "colombiana"]
+
+
+if(nacionalidades.includes(nacionalidade)){
+    console.log(nacionalidade)
+}else{
+    console.log("nacionalidade nao encontrada")
+}
+
